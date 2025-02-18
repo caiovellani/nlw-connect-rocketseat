00dc546e12b68d1de5d4c9a77de772c40afcb7e5
Caio Vellani <caiovellani@outlook.com>
2025-02-17 21:09:43 -0300

    Refactor: Move button components to root components directory
    - Deleted src/app/components/button.tsx and src/app/components/icon-button.tsx
    - Updated imports in src/app/page.tsx to use new component locations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { Button } from './components/button'
+import { Button } from '../components/button'
 import { ArrowRight, Copy } from 'lucide-react'
-import { IconButton } from './components/icon-button'
+import { IconButton } from '../components/icon-button'
 
 export default function Home() {
   return (
