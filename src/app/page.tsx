9e506e2309456998afd27ccec613f87fb6fb48b5
Caio Vellani <caiovellani@outlook.com>
2025-02-17 21:42:22 -0300

    feat: Add InputField component and update imports
    - Introduce new InputField, InputIcon, and InputRoot components
    - Update import paths to use '@/' alias for better organization
    - Add Mail icon and implement InputField in Home component
    - Remove unused ReactNode import from button and icon-button components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { Button } from '../components/button'
-import { ArrowRight, Copy } from 'lucide-react'
-import { IconButton } from '../components/icon-button'
+import { Button } from '@/components/button'
+import { ArrowRight, Copy, Mail } from 'lucide-react'
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
 
 export default function Home() {
   return (
@@ -13,6 +14,16 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail className="size-5" />
+          </InputIcon>
+
+          <InputField />
+        </InputRoot>
+      </div>
     </main>
   )
 }
